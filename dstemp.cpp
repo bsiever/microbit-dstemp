f99c0bd84a741e5fb42360f8adfb535b75cad755
Bill Siever <bsiever@gmail.com>
2020-12-06 11:48:09 -0600

    Test 1

--- dstemp.cpp
+++ dstemp.cpp
@@ -15,16 +15,24 @@
 #include "pxt.h"
 #include <cstdint>
 #include <math.h>
-#include "ble/BLE.h"
-#include "ble.h"
+// #include "ble/BLE.h"
+// #include "ble.h"
 #include "app_error.h"
-#include "mbed.h"
+// #include "mbed.h"
 #include "PinNames.h"
 #include "nrf.h"
-
+#include "MicroBitSystemTimer.h"
 #define DEBUG 0
 // DEBUG uses ioPin P1 to indicate sampling of read (for timing calibration)
 using namespace pxt;
+
+void wait_us(uint64_t delay) {
+    uint64_t start = system_timer_current_time();
+    while(system_timer_current_time()-start<delay) {
+        // Empty / Spin lock.        
+    }
+}
+
 
 namespace dstemp { 
 
